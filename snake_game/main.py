3c28cb6c9ab4782bacc1f870ab0b38da21a807e2
surya599 <suryabhuvaneshwarreddy@gmail.com>
2026-03-01 23:54:13 +0530

    animated the snake

--- snake_game/main.py
+++ snake_game/main.py
@@ -18,9 +18,13 @@ for positions in start_positions:
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(1)
-    for seg in segments:
-        seg.penup()
-        seg.forward(20)
+    time.sleep(0.1)
+    for seg_num in range(len(segments)-1,0,-1):
+        newx = segments[seg_num -1].xcor() 
+        newy = segments[seg_num -1].ycor() 
+        segments[seg_num].goto(newx,newy)
+    segments[0].forward(20)
+    segments[0].left(90)
+
 
 screen.exitonclick()
